737e52196c3c2958faf52c5021e6c6b1b0758d99
Unknown <867573@gmail.com>
2018-04-03 18:26:28 +0530

    components are aligned

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../css/App.css';
 
-class App extends Component {
+class Navbar extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
@@ -18,4 +17,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Navbar;
