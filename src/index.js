589a395e620419ece422cbba387b9f7e5b5a9225
Unknown <jamesstark145@gmail.com>
2018-04-03 18:39:09 +0530

    checking stark

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
 
-
 ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
