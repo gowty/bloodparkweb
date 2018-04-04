a050da4ae545b9349908334484600365a54d171f
Unknown <jamesstark145@gmail.com>
2018-04-04 18:29:36 +0530

    Navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import '../css/App.css';
+import '../css/custom.css';
 
 class Navbar extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand nav-brand" href="#">
+                <i class="fas fa-tint blood-icon"></i> <span>BloodPark</span>
+              </a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+              <ul class="nav navbar-nav navbar-right nav-content">
+                <li><a href="#" style={{color:"#fff"}}><i class="fas fa-reply"></i> Blood Request</a></li>
+                <li><a href="#" style={{color:"#fff"}}><i class="fas fa-heartbeat"></i> Become a Donor</a></li>
+                <li><a href="#" style={{color:"#fff"}}><i class="fab fa-hubspot"></i> About BloodPark</a></li>
+                <li><a href="#" style={{color:"#fff"}}><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
