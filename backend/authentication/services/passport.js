b0eb4c4e05cd538d1ccdd243bbf01d86f0f66661
Unknown <baymax1298@gmail.com>
2018-05-07 21:17:38 +0530

    login method changed
    user can login with email or username

--- backend/authentication/services/passport.js
+++ backend/authentication/services/passport.js
@@ -5,27 +5,53 @@ const jwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const LocalStrategy = require("passport-local");
 
+function validateEmail(email) {
+  var x = email;
+  var atpos = x.indexOf('@');
+  var dotpos = x.lastIndexOf('.');
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader("authorization"),
   secretOrKey: config.secret
 }
-
+ 
 const localOption = { usernameField:"email" }
 
 const localLogin = new LocalStrategy(localOption,function(email,password,done){
-
-   User.findOne({email:email},function(err,user){
-     if(err){return done(err)}
-     if(!user){return done(null,false)}
-
-     user.comparePassword(password, function(err,isMatch){
-       if(err){ return done(null) }
-       if(!isMatch){ return done(null,false)}
-
-       return done(null, user)
-     })
-
-   })
+   if(validateEmail(email)==true){
+    User.findOne({email:email},function(err,user){
+      if(err){return done(err)}
+      if(!user){return done(null,false)}
+ 
+      user.comparePassword(password, function(err,isMatch){
+        if(err){ return done(null) }
+        if(!isMatch){ return done(null,false)}
+ 
+        return done(null, user)
+      })
+ 
+    })
+   }else{
+    User.findOne({username:email},function(err,user){
+      if(err){return done(err)}
+      if(!user){return done(null,false)}
+ 
+      user.comparePassword(password, function(err,isMatch){
+        if(err){ return done(null) }
+        if(!isMatch){ return done(null,false)}
+ 
+        return done(null, user)
+      })
+ 
+    })
+   }
+   
 })
 
 const jwtLogin = new jwtStrategy(jwtOptions, function(payload, done){
@@ -41,5 +67,5 @@ const jwtLogin = new jwtStrategy(jwtOptions, function(payload, done){
 
 })
 
-passport.use(jwtLogin);
-passport.use(localLogin);
+passport.use("jwt",jwtLogin);
+passport.use("local",localLogin);
